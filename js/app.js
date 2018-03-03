b2f8ab0f315686412722235fa01352acb4e0f26a
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-03 22:57:48 +0530

    Unobtrusive JavaScript

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+// Create the "Reveal Spoiler" Button
+// $ sign before variable name is a great way to distinguish variables that contain selected elements from regular variables with other types of values.
+const $button = $('<button>Reveal Spoiler</button>'); 
+
+// Append to web page, it will insert at the end
+$('.spoiler').append($button);
+
+// To insert at the beginning 
+$('.spoiler').prepend($button);
+
+
 $('span').hide();
 
 $('button').click(function(){
@@ -6,4 +17,3 @@ $('button').click(function(){
   $('button').hide(3000);
 
 });
-
