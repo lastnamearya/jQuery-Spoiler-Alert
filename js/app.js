3885d45a9442b10e9399dfd558af3bc5e4303b6c
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-03 23:11:08 +0530

    on() Method

--- js/app.js
+++ js/app.js
@@ -5,13 +5,13 @@ const $button = $('<button>Reveal Spoiler</button>');
 // Append to web page, it will insert at the end
 $('.spoiler').append($button);
 
-// To insert at the beginning 
-$('.spoiler').prepend($button);
+// // To insert at the beginning 
+// $('.spoiler').prepend($button);
 
 
 $('span').hide();
 
-$('button').click(function(){
+$('button').on('click' , function(){
   $('span').show(1000);
   $('button').slideDown(1000);
   $('button').hide(3000);
