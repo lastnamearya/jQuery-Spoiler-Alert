6611cabac8fbc9b6656ec969d906fb399d101470
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-03 23:54:51 +0530

    event delegation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+// Event Delegation on parent element, before it was $('.spoiler button')
+$('.spoiler').on('click', 'button', function(){
+  $('.spoiler span').show(1000);
+  $('.spoiler button').slideDown(1000);
+  $('.spoiler button').hide(3000);
+})
+
+
+// ****************************************** //
+
 // Create the "Reveal Spoiler" Button
 // $ sign before variable name is a great way to distinguish variables that contain selected elements from regular variables with other types of values.
 const $button = $('<button>Reveal Spoiler</button>'); 
@@ -11,9 +21,9 @@ $('.spoiler').append($button);
 
 $('span').hide();
 
-$('button').on('click' , function(){
-  $('span').show(1000);
-  $('button').slideDown(1000);
-  $('button').hide(3000);
+// $('.spoiler button').click(function(){
+//   $('.spoiler span').show(1000);
+//   $('.spoiler button').slideDown(1000);
+//   $('.spoiler button').hide(3000);
 
-});
+// });
